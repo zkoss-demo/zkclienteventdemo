a7f9cfba97efea720500530044c61d65980609fa
mduchemin <matthieu@potix.com>
2020-03-17 19:30:34 +0800

    move to custom widgets

--- src/main/java/org/zkoss/example/ClientEventComposer.java
+++ src/main/java/org/zkoss/example/ClientEventComposer.java
@@ -30,36 +30,23 @@ public class ClientEventComposer extends SelectorComposer<Component> {
 	private Textbox tb8;
 	@Wire
 	private Textbox tb9;
-	
+
 	@Wire
 	private Textbox tb10;
-	
-
 	
 	@Override
 	public void doAfterCompose(Component comp) throws Exception {
 		super.doAfterCompose(comp);
 		tb1.setAttribute("next", tb2);
 		tb2.setAttribute("next", tb3);
-		tb3.setAttribute("next", tb1);
 	
-		initGroup2ClientListeners();
-		initGroup3ClientListeners();
-		
-	}
-	
-	public void initGroup2ClientListeners() {
-		String script = "function(event){doCustomKeyDownEnter_(event,this,$(\"$%s\"))}";
-		tb4.setWidgetOverride("doKeyDown_", String.format(script, "tb5"));
-		tb5.setWidgetOverride("doKeyDown_", String.format(script, "tb6"));
-		tb6.setWidgetOverride("doKeyDown_", String.format(script, "tb4"));
-		tb10.setWidgetOverride("doKeyDown_","function(event){interceptCtrlShiftFunctionKey_(event,this)}");
-	}
-	public void initGroup3ClientListeners() {
-		String script = "function(event){doBankEntryFormCheck(event,this,%s,%s)}";
-		tb7.setWidgetOverride("doKeyDown_", String.format(script, "$(\"$tb8\")","null"));
-		tb8.setWidgetOverride("doKeyDown_", String.format(script, "$(\"$tb9\")","$(\"$tb7\")"));
-		tb9.setWidgetOverride("doKeyDown_", String.format(script, "null","$(\"$tb8\")"));
+		tb4.setWidgetOverride("_next", "'$tb5'");
+		tb5.setWidgetOverride("_next", "'$tb6'");
+
+		tb7.setWidgetOverride("_next", "'$tb8'");
+		tb8.setWidgetOverride("_previous", "'$tb7'");
+		tb8.setWidgetOverride("_next", "'$tb9'");
+		tb9.setWidgetOverride("_previous", "'$tb8'");
 	}
 	
 	@Listen("onOK=#group1 > textbox")
@@ -74,6 +61,5 @@ public class ClientEventComposer extends SelectorComposer<Component> {
 	public void handleCustomKeyEvent(Event event) {
 		Clients.log("can use this event from server listener");
 	}
-	
 	
 }
